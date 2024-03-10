339b7e10f8f4f9bf7ca06c5ed0784fbc65a98efd
Maes95 <michel.maes95@gmail.com>
2024-03-10 23:34:11 +0100

    v11: For method scoreToTextNotEqualsAndLessThan4() extract the mapping from score to an aux function

--- Refactored/src/main/java/TennisGame1.java
+++ Refactored/src/main/java/TennisGame1.java
@@ -68,20 +68,25 @@ public class TennisGame1 implements TennisGame {
                 score += "-";
                 tempScore = m_score2;
             }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+            score = scoreToTextAux(score, tempScore);
+        }
+        return score;
+    }
+
+    private String scoreToTextAux(String score, int tempScore) {
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
         }
         return score;
     }
